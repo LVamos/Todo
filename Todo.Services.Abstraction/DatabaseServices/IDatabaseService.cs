9416fa8e3c103e6eaf551320b67f12e021a51fc2
Lukáš Vámoš <lukas.vamos@gmail.com>
2025-03-05 16:26:54 +0100

    *feat: extend DatabaseService

--- Todo.Services.Abstraction/DatabaseServices/IDatabaseService.cs
+++ Todo.Services.Abstraction/DatabaseServices/IDatabaseService.cs
@@ -1,5 +1,19 @@
-﻿namespace Todo.Services.Abstraction.DatabaseServices;
+﻿using Todo.Entities.Entities;
+
+namespace Todo.Services.Abstraction.DatabaseServices;
 
 public interface IDatabaseService
 {
+	Task<IEnumerable<TodoList>> GetAllTodoListsAsync();
+	Task<TodoList?> GetTodoListByIdAsync(int id);
+	Task AddTodoListAsync(TodoList todoList);
+	Task UpdateTodoListAsync(TodoList todoList);
+	Task DeleteTodoListAsync(int id);
+
+	// Operace s TodoItem
+	Task<IEnumerable<TodoItem>> GetTodoItemsByListIdAsync(int todoListId);
+	Task<TodoItem?> GetTodoItemByIdAsync(int id);
+	Task AddTodoItemAsync(TodoItem todoItem);
+	Task UpdateTodoItemAsync(TodoItem todoItem);
+	Task DeleteTodoItemAsync(int id);
 }
--- Todo.Services/DatabaseServices/DatabaseService.cs
+++ Todo.Services/DatabaseServices/DatabaseService.cs
@@ -1,14 +1,97 @@
-﻿using Todo.Dal.Abstraction;
+﻿using Microsoft.EntityFrameworkCore;
+
+using Todo.Dal.Abstraction;
+using Todo.Dal.Context;
+using Todo.Entities.Entities;
 using Todo.Services.Abstraction.DatabaseServices;
 
 namespace Todo.Services.DatabaseServices;
 
 public class DatabaseService : IDatabaseService
 {
-	public DatabaseService(IContextFactory contextFactory)
+	public async Task<IEnumerable<TodoList>> GetAllTodoListsAsync()
 	{
-		_contextFactory = contextFactory;
+		using TodoDbContext context = contextFactory.GetDbContext();
+		List<TodoList> lists = await context.TodoLists.Include(tl => tl.Items).ToListAsync();
+		return lists;
 	}
 
-	private IContextFactory _contextFactory;
+	public async Task<TodoList?> GetTodoListByIdAsync(int id)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		TodoList list = await context.TodoLists.Include(tl => tl.Items)
+								.FirstOrDefaultAsync(tl => tl.Id == id);
+		return list;
+	}
+
+	public async Task AddTodoListAsync(TodoList todoList)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		context.TodoLists.Add(todoList);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task UpdateTodoListAsync(TodoList todoList)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		context.TodoLists.Update(todoList);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task DeleteTodoListAsync(int id)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		TodoList list = await context.TodoLists.FindAsync(id);
+		if (list != null)
+		{
+			context.TodoLists.Remove(list);
+			await context.SaveChangesAsync();
+		}
+	}
+
+	public async Task<IEnumerable<TodoItem>> GetTodoItemsByListIdAsync(int todoListId)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		List<TodoItem> items = await context.TodoItems.Where(ti => ti.TodoListId == todoListId).ToListAsync();
+		return items;
+	}
+
+	public async Task<TodoItem?> GetTodoItemByIdAsync(int id)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		TodoItem item = await context.TodoItems.FirstOrDefaultAsync(ti => ti.Id == id);
+		return item;
+	}
+
+	public async Task AddTodoItemAsync(TodoItem todoItem)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		context.TodoItems.Add(todoItem);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task UpdateTodoItemAsync(TodoItem todoItem)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		context.TodoItems.Update(todoItem);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task DeleteTodoItemAsync(int id)
+	{
+		using TodoDbContext context = contextFactory.GetDbContext();
+		TodoItem item = await context.TodoItems.FindAsync(id);
+		if (item != null)
+		{
+			context.TodoItems.Remove(item);
+			await context.SaveChangesAsync();
+		}
+	}
+
+	public DatabaseService(IContextFactory contextFactory)
+	{
+		contextFactory = contextFactory;
+	}
+
+	private IContextFactory contextFactory;
 }
