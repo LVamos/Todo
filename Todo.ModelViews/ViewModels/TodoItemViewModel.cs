d2f7e2652c4132c98048c4d72552cff196c0206d
Lukáš Vámoš <lukas.vamos@gmail.com>
2025-07-17 12:15:17 +0200

    *chor: add view models

---
+++ Todo.ModelViews/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Todo.Entities.Entities;
+
+namespace Todo.ViewModels.ViewModels;
+
+public class TodoItemViewModel
+{
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public bool IsCompleted { get; set; }
+	public Priority Priority { get; set; }
+	public DateTime? Deadline { get; set; }
+	public string Comment { get; set; }
+	public int TodoListId { get; set; }
+}
---
+++ Todo.ModelViews/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Todo.ViewModels.ViewModels;
+
+public class TodoListViewModel
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+}
