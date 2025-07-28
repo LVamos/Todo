ecc9f7aae33c6aeeb86c86cd3cff56c0891de596
Lukáš Vámoš <lukas.vamos@gmail.com>
2025-07-28 12:10:03 +0200

    *feat: add controllers

---
+++ Todo.Api/Controllers/TodoItemsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Todo.Services.Abstraction.Services;
+using Todo.ViewModels.ViewModels;
+
+namespace Todo.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TodoItemsController : ControllerBase
+	{
+		private readonly ITodoItemService _todoItemService;
+
+		public TodoItemsController(ITodoItemService todoItemService)
+		{
+			_todoItemService = todoItemService;
+		}
+
+		[HttpGet("list/{listId}")]
+		[ProducesResponseType(typeof(IEnumerable<TodoItemViewModel>), 200)]
+		public async Task<ActionResult<IEnumerable<TodoItemViewModel>>> GetTodoItemsByListId(int listId)
+		{
+			IEnumerable<TodoItemViewModel> items = await _todoItemService.GetTodoItemsByListIdAsync(listId);
+			return Ok(items);
+		}
+
+		[HttpGet("{id}")]
+		[ProducesResponseType(typeof(TodoItemViewModel), 200)]
+		[ProducesResponseType(404)]
+		public async Task<ActionResult<TodoItemViewModel>> GetTodoItemById(int id)
+		{
+			TodoItemViewModel? item = await _todoItemService.GetTodoItemByIdAsync(id);
+			if (item == null)
+				return NotFound();
+			return Ok(item);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(201)]
+		public async Task<ActionResult> AddTodoItem([FromBody] TodoItemViewModel todoItem)
+		{
+			await _todoItemService.AddTodoItemAsync(todoItem);
+			return CreatedAtAction(nameof(GetTodoItemById), new { id = todoItem.Id }, todoItem);
+		}
+
+		[HttpPut("{id}")]
+		[ProducesResponseType(204)]
+		public async Task<ActionResult> UpdateTodoItem(int id, [FromBody] TodoItemViewModel todoItem)
+		{
+			if (id != todoItem.Id)
+				return BadRequest();
+			await _todoItemService.UpdateTodoItemAsync(todoItem);
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		[ProducesResponseType(204)]
+		public async Task<ActionResult> DeleteTodoItem(int id)
+		{
+			await _todoItemService.DeleteTodoItemAsync(id);
+			return NoContent();
+		}
+	}
+}
---
+++ Todo.Api/Controllers/TodoListsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Todo.Services.Abstraction.Services;
+using Todo.ViewModels.ViewModels;
+
+namespace Todo.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TodoListsController : ControllerBase
+	{
+		private readonly ITodoListService _todoListService;
+
+		public TodoListsController(ITodoListService todoListService)
+		{
+			_todoListService = todoListService;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<TodoListViewModel>), 200)]
+		public async Task<ActionResult<IEnumerable<TodoListViewModel>>> GetAllTodoLists()
+		{
+			IEnumerable<TodoListViewModel> lists = await _todoListService.GetAllTodoListsAsync();
+			return Ok(lists);
+		}
+
+		[HttpGet("{id}")]
+		[ProducesResponseType(typeof(TodoListViewModel), 200)]
+		[ProducesResponseType(404)]
+		public async Task<ActionResult<TodoListViewModel>> GetTodoListById(int id)
+		{
+			TodoListViewModel? list = await _todoListService.GetTodoListByIdAsync(id);
+			if (list == null)
+				return NotFound();
+			return Ok(list);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(201)]
+		public async Task<ActionResult> AddTodoList([FromBody] TodoListViewModel todoList)
+		{
+			await _todoListService.AddTodoListAsync(todoList);
+			return CreatedAtAction(nameof(GetTodoListById), new { id = todoList.Id }, todoList);
+		}
+
+		[HttpPut("{id}")]
+		[ProducesResponseType(204)]
+		public async Task<ActionResult> UpdateTodoList(int id, [FromBody] TodoListViewModel todoList)
+		{
+			if (id != todoList.Id)
+				return BadRequest();
+			await _todoListService.UpdateTodoListAsync(todoList);
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		[ProducesResponseType(204)]
+		public async Task<ActionResult> DeleteTodoList(int id)
+		{
+			await _todoListService.DeleteTodoListAsync(id);
+			return NoContent();
+		}
+	}
+}
