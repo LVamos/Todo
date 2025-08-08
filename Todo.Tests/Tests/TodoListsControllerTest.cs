f9769dcf83826110d48a2543f0042dbaa0ad5d17
Lukáš Vámoš <lukas.vamos@gmail.com>
2025-08-08 09:20:30 +0200

    *test: add GetAllTodoLists test for valid values

--- Todo.Tests/Tests/TodoListsControllerTest.cs
+++ Todo.Tests/Tests/TodoListsControllerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 using NUnit.Framework;
 
@@ -13,6 +14,20 @@ namespace Todo.Tests.Tests;
 [TestFixture]
 public class TodoListsControllerTest
 {
+    [Test]
+    public async Task GetAllTodoListsTestValid()
+    {
+        TestData.CurrentScenario = ResultType.Valid;
+        String uri = "api/TodoLists/GetAllTodoLists";
+        await TestPlatform.Client.GetAsync(uri);
+        TodoListsViewModel result = TestResults.GetAllTodoLists;
+    Assert.That(result, Is.Not.Null);
+        Assert.That(result.Lists, Is.Not.Null);
+        Assert.That(result.Lists.Count, Is.GreaterThan(1));
+        Assert.That(result.Lists[0].Name, Is.EqualTo("Shopping"));
+        Assert.That(result.Lists.Any(l => l.Id == 2), Is.True);
+    }
+
     [Test]
     public async Task GetAllTodoListsTestInvalid()
     {
