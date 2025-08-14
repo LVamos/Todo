3c8a288c15c9dfacf53f8c50927134953f1fb4a0
Lukáš Vámoš <lukas.vamos@gmail.com>
2025-08-14 17:54:50 +0200

    *feat: wrap up the app and database into Docker

--- Todo.App/Program.cs
+++ Todo.App/Program.cs
@@ -1,30 +1,32 @@
 using Autofac.Extensions.DependencyInjection;
-
 using Serilog;
-
 using Todo.App;
 using Todo.App.Helpers;
 using Todo.Dal.Abstraction;
 using Todo.Dal.Context;
 using Todo.ServerConfigurations.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 IConfiguration configuration = ConfigurationHelper.GetConfiguration(builder.Environment);
 
-// using autoFAC instead of standard di
+// using Autofac instead of standard DI
 builder.Host.UseServiceProviderFactory(
-	new AutofacServiceProviderFactory(x => new IocMapping().Install(configuration, x)));
+    new AutofacServiceProviderFactory(x => new IocMapping().Install(configuration, x)));
 
 Startup startup = new(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
 WebApplication app = builder.Build();
 IocContainerProvider.ServiceProvider = app.Services;
-IContextFactory factory = app.Services.GetService<IContextFactory>();
-using (TodoDbContext dbContext = factory.GetDbContext())
+
+// Apply EF Core migrations automatically on startup
+using (var scope = app.Services.CreateScope())
 {
-	dbContext.Database.EnsureCreated();
+    var factory = scope.ServiceProvider.GetRequiredService<IContextFactory>();
+    using var dbContext = factory.GetDbContext();
+    dbContext.Database.Migrate();
 }
 
 startup.Configure(app, app.Environment);
